21e24f726d9da023b7b95d3b43459e2f907e9a92
sharjeel65 <sharjeelahmadstanadar@gmail.com>
2022-03-21 18:06:14 +0500

    Initial commit

---
+++ lib/intro.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'home.dart';
+import 'package:lottie/lottie.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> with TickerProviderStateMixin {
+  late AnimationController _lottieAnimation;
+  var expanded = false;
+  final double _bigFontSize = kIsWeb ? 234 : 178; //if compiles on web size is 234 else 178
+  final transitionDuration = const Duration(seconds: 1);
+
+  @override
+  void initState() {
+    _lottieAnimation = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    Future.delayed(const Duration(seconds: 1))
+        .then((value) => setState(() => expanded = true))
+        .then((value) => const Duration(seconds: 1))
+        .then(
+          (value) => Future.delayed(const Duration(seconds: 1)).then(
+            (value) => _lottieAnimation.forward().then(
+                  (value) => Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                      (route) => false),
+                ),
+          ),
+        );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              stops: [
+                0.052,
+                0.72,
+                0.96,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFFb0b4e5),
+                Color(0xFF202c77),
+                Color(0xFF141468),
+              ],
+            ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedDefaultTextStyle(
+              duration: transitionDuration,
+              curve: Curves.fastOutSlowIn,
+              style: TextStyle(
+                color: const Color(0xFFCDCDD8),
+                fontSize: !expanded ? _bigFontSize : 50,
+                fontFamily: 'bauhaus',
+              ),
+              child: const Text(
+                "C",
+              ),
+            ),
+            AnimatedCrossFade(
+              firstCurve: Curves.fastOutSlowIn,
+              crossFadeState: !expanded
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: transitionDuration,
+              firstChild: Container(),
+              secondChild: _logoRemainder(),
+              alignment: Alignment.centerLeft,
+              sizeCurve: Curves.easeInOut,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _logoRemainder() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Text(
+          "HAT UP",
+          style: TextStyle(
+            color: Color(0xFFCDCDD8),
+            fontSize: 50,
+            fontFamily: 'bauhaus',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        LottieBuilder.asset(
+          'assets/Animations/1.json',
+          onLoaded: (composition) {
+            _lottieAnimation.duration = composition.duration;
+          },
+          frameRate: FrameRate.max,
+          repeat: false,
+          animate: false,
+          height: 100,
+          width: 100,
+          controller: _lottieAnimation,
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart'; //splash screen import
+import 'intro.dart';
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    // ignore_for_file: avoid_print
+
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready....');
+    FlutterNativeSplash.remove();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: IntroPage(),
+    );
+  }
+}
